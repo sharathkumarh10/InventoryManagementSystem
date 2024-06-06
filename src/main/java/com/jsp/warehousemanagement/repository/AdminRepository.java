e816855023c7dd6e878602f3304596c461de39e2
sharathkumarh10 <sharathkumarh1402@gmail.com>
2024-06-06 17:35:55 +0530

    Project updated

--- src/main/java/com/jsp/warehousemanagement/repository/AdminRepository.java
+++ src/main/java/com/jsp/warehousemanagement/repository/AdminRepository.java
@@ -1,5 +1,7 @@
 package com.jsp.warehousemanagement.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.jsp.warehousemanagement.entity.Admin;
@@ -9,5 +11,7 @@ import com.jsp.warehousemanagement.enums.AdminType;
 public interface AdminRepository extends JpaRepository<Admin,Integer > {
 	
 	public boolean existsByAdminType(AdminType adminType);
+
+	public Optional<Admin> findByEmail(String username);
 
 }
